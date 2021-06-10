edf0fe5d35e6097a420770c4a118edbabf5a1f0d
muhammadumair11 <muhammadumairmuhammadumair11@gmail.com>
2021-06-10 18:12:37 +0500

    newProject

---
+++ project 4/script.js
@@ -0,0 +1,16 @@
+const jokeEl = document.getElementById("joke");
+const btn = document.getElementById("jokebtn");
+
+dataJoke();
+btn.addEventListener("click", dataJoke);
+
+async function dataJoke() {
+	const config = {
+		headers: {
+			Accept: "application/json",
+		},
+	};
+	const res = await fetch("https://icanhazdadjoke.com", config);
+	const data = await res.json();
+	jokeEl.innerText = data.joke;
+}
