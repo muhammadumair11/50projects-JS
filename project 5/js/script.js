e88f6d9dab8afa76e7d5982a89f0cb073f2c5a05
muhammadumair11 <muhammadumairmuhammadumair11@gmail.com>
2021-06-11 17:20:45 +0500

    edit

--- project 5/js/script.js
+++ project 5/js/script.js
@@ -25,50 +25,44 @@ function update(current, location) {
 }
 
 btn.addEventListener("click", () => {
-	let city = input.value.toLowerCase();
+    dataFunc();
+});
 
+const dataFunc = async () => {
+    let city = input.value.toLowerCase();
 	if (city) {
-		fetch(
-			`http://api.weatherapi.com/v1/current.json?key=92fc14d1c7824494a33191055211006&q=${city}&aqi=yes`,
-			{
-				method: "GET",
-				headers: {
-					"Transfer-Encoding": "chunked",
-					Connection: "keep-alive",
-					Vary: "Accept-Encoding",
-					"CDN-PullZone": "93447",
-					"CDN-Uid": "8fa3a04a-75d9-4707-8056-b7b33c8ac7fe",
-					"CDN-RequestCountryCode": "FI",
-					"CDN-EdgeStorageId": "615",
-					"Request-Context":
-						"appId=cid-v1:89996683-9a04-40b3-8e46-77754119dcf5",
-					"CDN-CachedAt": "2021-06-10 21:13:42",
-					"CDN-RequestPullSuccess": "True",
-					"CDN-RequestPullCode": "200",
-					"CDN-RequestId": "9e4bdf7213ed3a84ba731a5d53d58a6b",
-					"CDN-Cache": "MISS",
-					"Cache-Control": "public, max-age=180",
-					"Content-Type": "application/json",
-					Server: "BunnyCDN-FI1-615",
-				},
-			}
-		)
-			.then((response) =>
-				response.status != 200
-					? console.log("Connection Failed")
-					: response.json()
-			)
-			.then((data) => {
-				let { current, location } = data;
-				update(current, location);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
+		const config = {
+			method: "GET",
+			headers: {
+				"Transfer-Encoding": "chunked",
+				Connection: "keep-alive",
+				Vary: "Accept-Encoding",
+				"CDN-PullZone": "93447",
+				"CDN-Uid": "8fa3a04a-75d9-4707-8056-b7b33c8ac7fe",
+				"CDN-RequestCountryCode": "FI",
+				"CDN-EdgeStorageId": "615",
+				"Request-Context": "appId=cid-v1:89996683-9a04-40b3-8e46-77754119dcf5",
+				"CDN-CachedAt": "2021-06-10 21:13:42",
+				"CDN-RequestPullSuccess": "True",
+				"CDN-RequestPullCode": "200",
+				"CDN-RequestId": "9e4bdf7213ed3a84ba731a5d53d58a6b",
+				"CDN-Cache": "MISS",
+				"Cache-Control": "public, max-age=180",
+				"Content-Type": "application/json",
+				Server: "BunnyCDN-FI1-615",
+			},
+		};
+
+		const response = await fetch(
+			`http://api.weatherapi.com/v1/current.json?key=92fc14d1c7824494a33191055211006&q=${city}&aqi=yes`, config)
+			
+            const data = await response.json();
+            let { current, location } = data;
+            update(current, location);
 	} else {
 		document.querySelector(".error").innerText = "Please Insert a City Name";
 		setTimeout(() => {
 			document.querySelector(".error").innerText = "";
 		}, 2000);
 	}
-});
+};
